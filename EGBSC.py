35fa28e0372d12b43836b09fccda06d7a1713c14
Scarlett <scarlett.wright@griffinschool.org>
2021-07-06 22:53:11 -0600

    F

--- EGBSC.py
+++ EGBSC.py
@@ -470,7 +470,7 @@ async def apiref(ctx):
 
 @client.command()
 async def help(ctx):
-  await ctx.send("```\nCommands:\n$stq - Sun Tzu Quotes\n$twitch - Twitch link\n$call - @ here\n$phys - Scientific papers link\n$navyseals - Navy Seals Copypasta\n$adam - Adam Wilson\n$vcroulette - Coming soon\n$rejoin/$rejoinbtp - Discord invite link\n$APIref - Documentation\n$join/leave - Lets the bot join/leave your current vc\n$vcHelp - Displays help for voice channel commands\n$tttHelp - Displays help for tic tac toe commands\n$alice - Pictures of Jacob's cat\n$clips - Clips channel link\n$lenny - Posts a random lenny face\n$sources - Links to sources I used to create this bot\n$useless - This command is useless, do not use it.\n$doge - Posts a doge, however there's a chance for a rare or ultra rare doge\n$report - Discord report link\nPrefixes:\nHurr/Durr - Hurrguy/Durrguy\nRed sus - red sus before\nWhen the imposter is sus - sus!\nI'm - Dad joke\nPog - WOO BABY\nP(a)edophile - Uno reverse\nYa\'ll - Corrects your ignorant mistake\nSussy - sussus amogus\nKorea - Korea spam```")
+  await ctx.send("```\nCommands:\n$stq - Sun Tzu Quotes\n$twitch - Twitch link\n$call - @ here\n$phys - Scientific papers link\n$navyseals - Navy Seals Copypasta\n$adam - Adam Wilson\n$vcroulette - Coming soon\n$rejoin/$rejoinbtp - Discord invite link\n$APIref - Documentation\n$join/leave - Lets the bot join/leave your current vc\n$vcHelp - Displays help for voice channel commands\n$tttHelp - Displays help for tic tac toe commands\n$alice - Pictures of Jacob's cat\n$clips - Clips channel link\n$lenny - Posts a random lenny face\n$sources - Links to sources I used to create this bot\n$useless - This command is useless, do not use it.\n$doge - Posts a doge, however there's a chance for a rare or ultra rare doge\n$report - Discord report link\n$rng (number) - Chooses a random number between 1 and the entered number\nPrefixes:\nHurr/Durr - Hurrguy/Durrguy\nRed sus - red sus before\nWhen the imposter is sus - sus!\nI'm - Dad joke\nPog - WOO BABY\nP(a)edophile - Uno reverse\nYa\'ll - Corrects your ignorant mistake\nSussy - sussus amogus\nKorea - Korea spam```")
 
 @client.command()
 async def clips(ctx):
@@ -478,11 +478,11 @@ async def clips(ctx):
 
 @client.command()
 async def updateBot(ctx):
-  await ctx.send("Bot updated to 1.6.5!")
+  await ctx.send("Bot updated to 1.6.6!")
 
 @client.command()
 async def changelog(ctx):
-  await ctx.send("1.6.5: created $dmTest, updated $help")
+  await ctx.send("1.6.6: created $rng, updated $help")
 
 @client.command()
 async def useless(ctx, amount=1):
@@ -507,6 +507,8 @@ async def report(ctx):
 async def rng(ctx, inputNum : str):
   num = random.randint(1, inputNum)
   await ctx.send(f"{num}")
+  if isinstance(error, commands.MissingRequiredArgument):
+    await ctx.send("Put a number!")
 
 # vc commands----------------------------------------------
 @client.command()
@@ -846,6 +848,7 @@ client.run('ODAyMjU2ODY3Mjg4MDIzMDUx.YAsl7g.5Z6E_SyEnKzj-DHPBITA0FKYJ94')
 # 1.6.3: removed $breakMe
 # 1.6.4: created $report
 # 1.6.5: created $dmTest, updated $help
+# 1.6.6: created $rng, updated $help
 
 #sources: 
 # discord.py discord
