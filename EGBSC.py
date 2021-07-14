e526ed47993e3f2c251822d2039287d13ced1179
Scarlett <scarlett.wright@griffinschool.org>
2021-07-13 19:18:22 -0600

    G

--- EGBSC.py
+++ EGBSC.py
@@ -525,12 +525,12 @@ async def rng_error(ctx, error):
 # vc commands----------------------------------------------
 @client.command()
 async def join(ctx):
-    if (ctx.author.voice):
-      channel = ctx.message.author.voice.channel
-      await channel.connect()
-      await ctx.send("Joined the voice channel!")
-    else:
-      await ctx.send("You\'re not in a voice channel!")
+  if (ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+    await ctx.send("Joined the voice channel!")
+  else:
+    await ctx.send("You\'re not in a voice channel!")
 # join vc command
 
 @client.command()
