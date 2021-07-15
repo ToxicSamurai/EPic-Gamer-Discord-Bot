8e67d14409003b49afc7c9deacbbe99de0555332
Scarlett <scarlett.wright@griffinschool.org>
2021-07-15 02:33:09 -0600

    F

--- EGBSC.py
+++ EGBSC.py
@@ -512,7 +512,7 @@ async def updateBot(ctx):
 
 @client.command()
 async def changelog(ctx):
-  await ctx.send("1.6.10: reorganized indentation on stqDict, created $deadchat, created dict for $deadchat (deadChatDict), updated $help, deleted song.mp3 from local (7/15/21)")
+  await ctx.send("1.6.10: (updated changelog) reorganized indentation on stqDict, created $deadchat, created dict for $deadchat (deadChatDict), updated $help, deleted song.mp3 from local (7/15/21)")
 
 @client.command()
 async def useless(ctx, amount=1):
@@ -930,7 +930,7 @@ client.run('ODAyMjU2ODY3Mjg4MDIzMDUx.YAsl7g.5Z6E_SyEnKzj-DHPBITA0FKYJ94')
 # 1.6.7: created error handler for $rng (@rng.error() - rngErrorHandler)
 # 1.6.8: fixed and edited error handler for $rng (@rng.error - rng_error) (7/7/21)
 # 1.6.9: allowed server members intent, updated $sources (7/12/21)
-# 1.6.10: reorganized indentation on stqDict, created $deadchat, created dict for $deadchat (deadChatDict), updated $help, deleted song.mp3 from local (7/15/21)
+# 1.6.10: (updated changelog) reorganized indentation on stqDict, created $deadchat, created dict for $deadchat (deadChatDict), updated $help, deleted song.mp3 from local (7/15/21)
 # 1.x: created $vcroullete and error handler (@vcroulette.error - vcroulette_error), updated $help, updated $souces
 # 1.x: created $suggest and error handler (@suggest.error - suggest_error), updated $help
 
